96214943142ae8fc3c7ad73735b3558fefbf36f1
sas4211 <sas4211@gmail.com>
2024-04-27 00:29:36 +0400

    word-counter

---
+++ index.js
@@ -0,0 +1,11 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter Your Sentence To Count The Word"
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+console.log(words);
+console.log(`Your Sentence Word Count Is ${words.length}`);
